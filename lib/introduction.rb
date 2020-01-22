f31981144846ed43f0fbdef08971ca9ac2be31ad
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 00:29:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,3 +4,9 @@ end
 
 introduction("Kelly")
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I a learning to program in #{language}."
+end
+
+introduction_with_language("Kelly", "Ruby")
+  
